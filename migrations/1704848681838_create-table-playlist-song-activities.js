ccae26b718e4ef791a3006cafb4463bad785de14
andhiyaulhaq <andhiyaulhaq@gmail.com>
2024-01-11 15:08:59 +0700

    first commit

---
+++ migrations/1704848681838_create-table-playlist-song-activities.js
@@ -0,0 +1,42 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('playlist_song_activities', {
+    id: {
+      type: 'SERIAL',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(25)',
+      notNull: true,
+      references: 'playlists(id)',
+      onDelete: 'CASCADE',
+    },
+    song_id: {
+      type: 'VARCHAR(25)',
+      notNull: true,
+      references: 'songs(id)',
+      onDelete: 'CASCADE',
+    },
+    user_id: {
+      type: 'VARCHAR(25)',
+      notNull: true,
+      references: 'users(id)',
+      onDelete: 'CASCADE',
+    },
+    action: {
+      type: 'VARCHAR(6)',
+      notNull: 'CASCADE',
+    },
+    time: {
+      type: 'TEXT',
+      notNull: 'CASCADE',
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('playlist_song_activities');
+};
---
+++ src/api/albums/handler.js
@@ -0,0 +1,69 @@
+const autoBind = require('auto-bind');
+
+class AlbumsHandler {
+  constructor(service, validator) {
+    this.service = service;
+    this.validator = validator;
+
+    autoBind(this);
+  }
+
+  async postAlbumHandler(request, h) {
+    this.validator.validateAlbumPayload(request.payload);
+    const { name, year } = request.payload;
+
+    const albumId = await this.service.addAlbum(name, year);
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        albumId,
+      },
+    });
+
+    response.code(201);
+    return response;
+  }
+
+  async getAlbumByIdHandler(request) {
+    const { id } = request.params;
+    const album = await this.service.getAlbumById(id);
+    const songs = await this.service.getSongsByAlbumId(id);
+
+    return {
+      status: 'success',
+      data: {
+        album: {
+          id: album.id,
+          name: album.name,
+          year: album.year,
+          songs,
+        },
+      },
+    };
+  }
+
+  async putAlbumByIdHandler(request) {
+    this.validator.validateAlbumPayload(request.payload);
+    const { id } = request.params;
+
+    await this.service.editAlbumById(id, request.payload);
+
+    return {
+      status: 'success',
+      message: 'Album berhasil diperbarui',
+    };
+  }
+
+  async deleteAlbumByIdHandler(request) {
+    const { id } = request.params;
+    await this.service.deleteAlbumById(id);
+
+    return {
+      status: 'success',
+      message: 'Album berhasil dihapus',
+    };
+  }
+}
+
+module.exports = AlbumsHandler;
